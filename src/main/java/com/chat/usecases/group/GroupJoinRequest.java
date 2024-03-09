bb09c1a25e47def11feebf056de088b89643de9c
yuuy83 <nghiawapro2@gmail.com>
2024-03-09 08:03:58 +0700

    eedit SendingRequest class

--- src/main/java/com/chat/usecases/group/GroupJoinRequest.java
+++ src/main/java/com/chat/usecases/group/GroupJoinRequest.java
@@ -17,36 +17,26 @@ public class GroupJoinRequest extends UseCase<GroupJoinRequest.InputValues, Grou
 	}
 
 	public static class InputValues {
-		private String _adminId;
 		private String _groupId;
 		private String _userSendRequestId;
 
-		public InputValues(String adminId, String groupId, String userSendRequestId) {
+		public InputValues(String groupId, String userSendRequestId) {
 			super();
-			this._adminId = adminId;
 			this._groupId = groupId;
 			this._userSendRequestId = userSendRequestId;
 		}
-
 	}
 
 	public static class OutputValues {
 		private GroupJoinRequestResult _result;
-		private String _message;
 
 		public OutputValues(GroupJoinRequestResult result, String message) {
 			this._result = result;
-			this._message = message;
 		}
 
 		public GroupJoinRequestResult getResult() {
 			return _result;
 		}
-
-		public String getMessage() {
-			return _message;
-		}
-
 	}
 
 	public static enum GroupJoinRequestResult {
@@ -59,29 +49,13 @@ public class GroupJoinRequest extends UseCase<GroupJoinRequest.InputValues, Grou
 		GroupRepository groupRepository = _dataStorage.getGroupRepository();
 
 		User userSendRequestId = userRepository.findById(input._userSendRequestId);
-		User admin = userRepository.findById(input._adminId);
 		Group inputGroup = groupRepository.findById(input._groupId);
 
-		if (userSendRequestId == null || inputGroup == null || admin == null) {
+		if (userSendRequestId == null || inputGroup == null) {
 			return new OutputValues(GroupJoinRequestResult.Failed, "");
-		}
-		boolean isApprove = false;
-		if (inputGroup instanceof PublicGroup) {
-			return new OutputValues(GroupJoinRequestResult.Failed, "");
-		}
-		isApprove = acceptByAdmin((PrivateGroup) inputGroup, userSendRequestId, admin);
-		if (isApprove) {
-			return new OutputValues(GroupJoinRequestResult.Successed, "");
 		} else {
-			return new OutputValues(GroupJoinRequestResult.Failed, "");
+			return new OutputValues(GroupJoinRequestResult.Successed, "");
 		}
 	}
 
-	private boolean acceptByAdmin(PrivateGroup inputGroup, User userSendRequestId, User admin) {
-		if (inputGroup.isAdmin(admin)) {
-			((PrivateGroup) inputGroup).addMember(userSendRequestId);
-			return true;
-		}
-		return false;
-	}
 }
